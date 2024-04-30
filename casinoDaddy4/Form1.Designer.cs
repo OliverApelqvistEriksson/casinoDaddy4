9c58810e3cfe42ee45803ad122f67f4ee8709126
Oliver Apelqvist Eriksson <oliape060414@student.jenseneducation.se>
2024-04-30 10:54:14 +0200

    Add project files.

---
+++ casinoDaddy4/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace casinoDaddy4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            slot1 = new PictureBox();
+            slot2 = new PictureBox();
+            slot3 = new PictureBox();
+            slot4 = new PictureBox();
+            slot5 = new PictureBox();
+            slot10 = new PictureBox();
+            slot9 = new PictureBox();
+            slot8 = new PictureBox();
+            slot7 = new PictureBox();
+            slot6 = new PictureBox();
+            slot15 = new PictureBox();
+            slot14 = new PictureBox();
+            slot13 = new PictureBox();
+            slot12 = new PictureBox();
+            slot11 = new PictureBox();
+            spinButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)slot1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot15).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)slot11).BeginInit();
+            SuspendLayout();
+            // 
+            // slot1
+            // 
+            slot1.Location = new Point(148, 56);
+            slot1.Name = "slot1";
+            slot1.Size = new Size(127, 117);
+            slot1.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot1.TabIndex = 0;
+            slot1.TabStop = false;
+            // 
+            // slot2
+            // 
+            slot2.Location = new Point(281, 56);
+            slot2.Name = "slot2";
+            slot2.Size = new Size(127, 117);
+            slot2.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot2.TabIndex = 1;
+            slot2.TabStop = false;
+            // 
+            // slot3
+            // 
+            slot3.Location = new Point(414, 56);
+            slot3.Name = "slot3";
+            slot3.Size = new Size(127, 117);
+            slot3.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot3.TabIndex = 2;
+            slot3.TabStop = false;
+            // 
+            // slot4
+            // 
+            slot4.Location = new Point(547, 56);
+            slot4.Name = "slot4";
+            slot4.Size = new Size(127, 117);
+            slot4.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot4.TabIndex = 3;
+            slot4.TabStop = false;
+            // 
+            // slot5
+            // 
+            slot5.Location = new Point(680, 56);
+            slot5.Name = "slot5";
+            slot5.Size = new Size(127, 117);
+            slot5.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot5.TabIndex = 4;
+            slot5.TabStop = false;
+            // 
+            // slot10
+            // 
+            slot10.Location = new Point(680, 179);
+            slot10.Name = "slot10";
+            slot10.Size = new Size(127, 117);
+            slot10.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot10.TabIndex = 9;
+            slot10.TabStop = false;
+            // 
+            // slot9
+            // 
+            slot9.Location = new Point(547, 179);
+            slot9.Name = "slot9";
+            slot9.Size = new Size(127, 117);
+            slot9.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot9.TabIndex = 8;
+            slot9.TabStop = false;
+            // 
+            // slot8
+            // 
+            slot8.Location = new Point(414, 179);
+            slot8.Name = "slot8";
+            slot8.Size = new Size(127, 117);
+            slot8.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot8.TabIndex = 7;
+            slot8.TabStop = false;
+            // 
+            // slot7
+            // 
+            slot7.Location = new Point(281, 179);
+            slot7.Name = "slot7";
+            slot7.Size = new Size(127, 117);
+            slot7.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot7.TabIndex = 6;
+            slot7.TabStop = false;
+            // 
+            // slot6
+            // 
+            slot6.Location = new Point(148, 179);
+            slot6.Name = "slot6";
+            slot6.Size = new Size(127, 117);
+            slot6.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot6.TabIndex = 5;
+            slot6.TabStop = false;
+            // 
+            // slot15
+            // 
+            slot15.Location = new Point(680, 302);
+            slot15.Name = "slot15";
+            slot15.Size = new Size(127, 117);
+            slot15.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot15.TabIndex = 14;
+            slot15.TabStop = false;
+            // 
+            // slot14
+            // 
+            slot14.Location = new Point(547, 302);
+            slot14.Name = "slot14";
+            slot14.Size = new Size(127, 117);
+            slot14.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot14.TabIndex = 13;
+            slot14.TabStop = false;
+            // 
+            // slot13
+            // 
+            slot13.Location = new Point(414, 302);
+            slot13.Name = "slot13";
+            slot13.Size = new Size(127, 117);
+            slot13.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot13.TabIndex = 12;
+            slot13.TabStop = false;
+            // 
+            // slot12
+            // 
+            slot12.Location = new Point(281, 302);
+            slot12.Name = "slot12";
+            slot12.Size = new Size(127, 117);
+            slot12.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot12.TabIndex = 11;
+            slot12.TabStop = false;
+            // 
+            // slot11
+            // 
+            slot11.Location = new Point(148, 302);
+            slot11.Name = "slot11";
+            slot11.Size = new Size(127, 117);
+            slot11.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot11.TabIndex = 10;
+            slot11.TabStop = false;
+            // 
+            // spinButton
+            // 
+            spinButton.Location = new Point(843, 86);
+            spinButton.Name = "spinButton";
+            spinButton.Size = new Size(112, 339);
+            spinButton.TabIndex = 15;
+            spinButton.Text = "button1";
+            spinButton.UseVisualStyleBackColor = true;
+            spinButton.Click += spinButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(999, 594);
+            Controls.Add(spinButton);
+            Controls.Add(slot15);
+            Controls.Add(slot14);
+            Controls.Add(slot13);
+            Controls.Add(slot12);
+            Controls.Add(slot11);
+            Controls.Add(slot10);
+            Controls.Add(slot9);
+            Controls.Add(slot8);
+            Controls.Add(slot7);
+            Controls.Add(slot6);
+            Controls.Add(slot5);
+            Controls.Add(slot4);
+            Controls.Add(slot3);
+            Controls.Add(slot2);
+            Controls.Add(slot1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)slot1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot15).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)slot11).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox slot1;
+        private PictureBox slot2;
+        private PictureBox slot3;
+        private PictureBox slot4;
+        private PictureBox slot5;
+        private PictureBox slot10;
+        private PictureBox slot9;
+        private PictureBox slot8;
+        private PictureBox slot7;
+        private PictureBox slot6;
+        private PictureBox slot15;
+        private PictureBox slot14;
+        private PictureBox slot13;
+        private PictureBox slot12;
+        private PictureBox slot11;
+        private Button spinButton;
+    }
+}
